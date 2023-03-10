933595be6781b1de052b37a685bb7daf1029560e
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-03-10 08:53:15 -0500

    feat(Solution Change): :memo: Solución ejercicio 4
    Desarrollo del ejercicio 4 del taller de JavaScript proporcionado por Campus

--- main.js
+++ main.js
@@ -1,10 +1,32 @@
-// 3. Construir el algoritmo para determinar el voltaje de un
-// circuito a partir de la resistencia y la intensidad de corriente. 
+// nombre y 
+// 4. Construir el algoritmo que solicite el 
+// edad 
+// de 3 personas y determine el 
+// nombre de la persona con mayor edad.
+let nombre = [];
+let edad = [];
+do{
+    nombre.push(prompt('ingrese nombre'));
+    edad.push(parseInt(prompt('ingrese edad')));
+}while(confirm('confirme para ingresar nuevos datos'));
 
-let resistencia = prompt("Ingrese el dato resistencia")
-let intensidad = prompt("Ingrese el dato intensidad")
-let voltaje = resistencia * intensidad
-alert("Resistencia = " + resistencia + "ax " +  "\nIntensidad de corriente = "+ intensidad + "ohms" + "\nVoltaje = " + voltaje + "v" )
+let edadMax = edad.indexOf(Math.max(...edad));
+console.log(`${nombre[edadMax]} tiene ${Math.max(...edad)} es el mayor`)
 
 
 
+// console.log(Math.max(lista))
+
+
+
+
+// numero mayor de edad
+// indice de la mayor edad
+// imprimir
+
+
+
+
+
+
+// lista.edad = prompt('ingrese edad')
