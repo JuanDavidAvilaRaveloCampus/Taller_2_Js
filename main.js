e6ee50ed16ca6cb7ffb59ee82d491dcd1d8af946
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-03-09 23:20:10 -0500

    feat(Solution Change): :memo: Solution 3
    Desarrollo de la solución #3 del taller de JavaScript

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-// 2. Dado un número indicar: 
-// si es par o impar y si es mayor de 10.
+// 3. Construir el algoritmo para 
+// determinar el voltaje de un circuito a partir de 
+// la resistencia y 
+// la intensidad de corriente.
 
-let no_Number = prompt("Ingrese un número: ");
-let number = parseInt(no_Number);
-let resto = number % 2; 
-if ( resto == 0  && number > 10){
-    alert("Su numero es par y es mayor a 10")
-} else if ( resto == 0  && number == 10){
-    alert("Su numero es par y es 10")
-} else if ( resto != 0  && number >= 10){
-    alert("Su numero no es par, pero es mayor a 10")
-} else if ( resto == 0  && number <= 9){
-    alert("Su numero es par y es menor a 10")
-} else if ( resto != 0  && number <= 9){
-    alert("Su numero es impar y es menor a 10")
-};
+let resistencia = prompt("Ingrese el valor de la resistencia del circuito: ")
+Number.resistencia
+let corriente = prompt("Ingrese el valor de la intensidad de corriente del circuito: ")
+Number.corriente
+let voltaje = corriente * resistencia
+
+alert("Resistencia = " + resistencia+ "ax" + "\nCorriente = " + corriente + "ohms" + "\nVoltaje = " + voltaje + "v")
